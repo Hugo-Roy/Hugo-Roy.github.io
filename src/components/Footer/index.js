851aec0eff852e6482ed505efce4b3b2a75d26e2
Hugo-Roy <roy.hugo21@gmail.com>
2021-05-21 10:07:29 +0200

    footer done

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,18 +1,29 @@
 /* eslint-disable jsx-a11y/control-has-associated-label */
 import React from 'react';
+import { motion } from 'framer-motion';
 import './footer.scss';
 
 const Footer = () => (
-  <div className="footer">
+  <footer className="footer">
     <ul className="footer__list">
-      <li className="footer__list__item">
-        <a className="footer__list__item__link" href="#"><i className="fab fa-linkedin" /></a>
-      </li>
-      <li className="footer__list__item">
-        <a className="footer__list__item__link" href="#"><i className="fab fa-github-square" /></a>
-      </li>
+      <motion.li
+        className="footer__list__item"
+        initial={{ y: 100 }}
+        animate={{ y: 0 }}
+        transition={{ duration: 0.2 }}
+      >
+        <a className="footer__list__item__link" href="https://www.linkedin.com/in/roy-hugo/"><i className="fab fa-linkedin" /></a>
+      </motion.li>
+      <motion.li
+        className="footer__list__item"
+        initial={{ y: 100 }}
+        animate={{ y: 0 }}
+        transition={{ duration: 0.4 }}
+      >
+        <a className="footer__list__item__link" href="https://github.com/Hugo-Roy"><i className="fab fa-github-square" /></a>
+      </motion.li>
     </ul>
-  </div>
+  </footer>
 );
 
 export default Footer;
